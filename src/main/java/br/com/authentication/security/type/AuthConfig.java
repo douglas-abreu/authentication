9ad3c0ed0622f7b62aa76eb1492f714ad443e1fc
Douglas <douglas.martins@parnamirim.rn.gov.br>
2024-10-28 16:09:43 -0300

    support: removed unnecessary constants

--- src/main/java/br/com/authentication/security/type/AuthConfig.java
+++ src/main/java/br/com/authentication/security/type/AuthConfig.java
@@ -4,8 +4,6 @@ import lombok.Getter;
 
 @Getter
 public enum AuthConfig {
-	GERENCIADOR_PRINCIPAL("UUtYGqgzPAoSZgFDIjNFdNky3l23XAAe"),
-	ATENTIMENTOS("4z7NgTHNQtmpbSxZeXREXbjCexFSCZTk"),
 	SECRET_KEY("UUtYGqgzPAoSZgFDIjNFdNky3l23XAZZ"),
 	EXPIRE_TIME("14400000");
 	
